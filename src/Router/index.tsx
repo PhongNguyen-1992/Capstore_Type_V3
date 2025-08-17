7211976c86b74fb7ccf1181926ef943efa639f19
PhongNguyen-1992 <phongnguyenfpt1992@gmail.com>
2025-08-17 16:34:08 +0700

    clear code1

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -1,15 +1,12 @@
-import path from "path";
-import React, {
+import {
   lazy,
   Suspense,
   type FC,
   type LazyExoticComponent,
 } from "react";
-import { Route, type RouteObject } from "react-router-dom";
+import {type RouteObject } from "react-router-dom";
 const Homepage = lazy(() => import("../Page/HomeTemplete/index"));
 const Login = lazy(() => import("../Page/AuthTemplete/Login"));
-const Movie = lazy(() => import("../Page/HomeTemplete/Movie/listmovie"));
-const TrangChu = lazy(() => import("../Page/HomeTemplete/TrangChu"));
 const Auth = lazy(() => import("../Page/AuthTemplete"));
 const MovieDetail = lazy(() => import("../Page/HomeTemplete/Movie/detail"));
 
