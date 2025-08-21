46568ad51175c781c01e9a8420bd1839c9134c25
PhongNguyen-1992 <phongnguyenfpt1992@gmail.com>
2025-08-21 15:29:20 +0700

    update MovieV3

--- src/Page/HomeTemplete/TicketBooking/index.tsx
+++ src/Page/HomeTemplete/TicketBooking/index.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import {  useParams } from "react-router-dom";
 import {
   Card,
   Button,
@@ -16,8 +16,7 @@ import {
   notification,
   Badge,
 } from "antd";
-import {
-  PlayCircleOutlined,
+import { 
   EnvironmentOutlined,
   CheckCircleOutlined,
   LoadingOutlined,
